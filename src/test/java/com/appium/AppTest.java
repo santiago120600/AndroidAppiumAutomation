4a7c6de4bf3dedbc813e0f20438b517cdbc021b7
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-03-14 12:32:20 -0600

    start emulator

--- src/test/java/com/appium/AppTest.java
+++ src/test/java/com/appium/AppTest.java
@@ -1,11 +1,14 @@
 package com.appium;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 import org.testng.annotations.Test;
 
@@ -16,19 +19,32 @@ import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public class AppTest {
     @Test
-    public void AppiumTest() throws MalformedURLException, URISyntaxException{
-        AppiumDriverLocalService  service = new AppiumServiceBuilder()
-        .withAppiumJS(new File("C://Users//santi//scoop//apps//nvm//current//nodejs//nodejs//node_modules//appium//build//lib//main.js"))
-        .withIPAddress("0.0.0.0").usingPort(4723).build();
+    public void AppiumTest() throws MalformedURLException, URISyntaxException {
+        Properties prop = new Properties();
+        try (
+            FileInputStream path = new FileInputStream("src\\test\\resources\\global.properties")) {
+            prop.load(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ProcessBuilder pb = new ProcessBuilder(prop.getProperty("emulator"), "-avd", prop.getProperty("deviceName"));
+        try {
+            pb.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        AppiumDriverLocalService service = new AppiumServiceBuilder()
+                .withAppiumJS(new File(prop.getProperty("appium")))
+                .withIPAddress("0.0.0.0").usingPort(4723).build();
         service.start();
 
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("Pixel_3");
+        options.setDeviceName(prop.getProperty("deviceName"));
         Path path = Paths.get("src/test/resources/ApiDemos-debug.apk");
         options.setApp(path.toAbsolutePath().toString());
         AndroidDriver driver = new AndroidDriver(new URI("http://0.0.0.0:4723").toURL(), options);
         driver.quit();
         service.stop();
     }
-   
+
 }
