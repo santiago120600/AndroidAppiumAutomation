af382087fd029b4b86932c991fa4104ed0990d75
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-03-14 13:26:31 -0600

    wait for emulator to be ready

--- src/test/java/com/appium/AppTest.java
+++ src/test/java/com/appium/AppTest.java
@@ -9,6 +9,8 @@ import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import org.testng.annotations.Test;
 
@@ -18,11 +20,31 @@ import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public class AppTest {
+
+    private boolean isEmulatorReady() {
+        ProcessBuilder processBuilder = new ProcessBuilder("adb", "shell", "getprop", "sys.boot_completed");
+        try {
+            Process process = processBuilder.start();
+            process.waitFor(10, TimeUnit.SECONDS);
+
+            // Read the output of the command
+            try (Scanner scanner = new Scanner(process.getInputStream())) {
+                if (scanner.hasNext()) {
+                    String output = scanner.next().trim();
+                    return output.equals("1");
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     @Test
     public void AppiumTest() throws MalformedURLException, URISyntaxException {
         Properties prop = new Properties();
         try (
-            FileInputStream path = new FileInputStream("src\\test\\resources\\global.properties")) {
+                FileInputStream path = new FileInputStream("src\\test\\resources\\global.properties")) {
             prop.load(path);
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,6 +54,13 @@ public class AppTest {
             pb.start();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        while (!isEmulatorReady()) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         AppiumDriverLocalService service = new AppiumServiceBuilder()
                 .withAppiumJS(new File(prop.getProperty("appium")))
@@ -46,7 +75,7 @@ public class AppTest {
         driver.quit();
         service.stop();
         ProcessBuilder closeEmulator = new ProcessBuilder("adb", "emu", "kill");
-        try{
+        try {
             closeEmulator.start();
         } catch (IOException e) {
             e.printStackTrace();
