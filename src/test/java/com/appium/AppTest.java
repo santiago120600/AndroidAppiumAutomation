eb6ea60fc386cea3adcbcf8ca4a9c4af20ed5fce
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-03-14 14:42:05 -0600

    refactor test setup

--- src/test/java/com/appium/AppTest.java
+++ src/test/java/com/appium/AppTest.java
@@ -12,6 +12,8 @@ import java.util.Properties;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
@@ -21,13 +23,48 @@ import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public class AppTest {
 
+    private AppiumDriverLocalService service;
+    private AndroidDriver driver;
+    private Properties prop;
+
+    @BeforeClass
+    public void setUp() throws IOException, URISyntaxException {
+        prop = new Properties();
+        try (FileInputStream path = new FileInputStream("src/test/resources/global.properties")) {
+            prop.load(path);
+        }
+
+        startEmulator();
+        startAppiumServer();
+        initializeDriver();
+    }
+
+    private void startEmulator() {
+        ProcessBuilder pb = new ProcessBuilder(prop.getProperty("emulator"), "-avd", prop.getProperty("deviceName"), "-wipe-data", "-no-window");
+        try {
+            pb.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to start emulator", e);
+        }
+
+        while (!isEmulatorReady()) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Interrupted while waiting for emulator to be ready", e);
+            }
+        }
+    }
+
     private boolean isEmulatorReady() {
         ProcessBuilder processBuilder = new ProcessBuilder("adb", "shell", "getprop", "sys.boot_completed");
         try {
             Process process = processBuilder.start();
             process.waitFor(10, TimeUnit.SECONDS);
 
-            // Read the output of the command
             try (Scanner scanner = new Scanner(process.getInputStream())) {
                 if (scanner.hasNext()) {
                     String output = scanner.next().trim();
@@ -36,49 +73,52 @@ public class AppTest {
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            throw new RuntimeException("Failed to check emulator status", e);
         }
         return false;
     }
 
-    @Test
-    public void AppiumTest() throws MalformedURLException, URISyntaxException {
-        Properties prop = new Properties();
-        try (
-                FileInputStream path = new FileInputStream("src\\test\\resources\\global.properties")) {
-            prop.load(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ProcessBuilder pb = new ProcessBuilder(prop.getProperty("emulator"), "-avd", prop.getProperty("deviceName"));
-        try {
-            pb.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        while (!isEmulatorReady()) {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        AppiumDriverLocalService service = new AppiumServiceBuilder()
+    private void startAppiumServer() {
+        service = new AppiumServiceBuilder()
                 .withAppiumJS(new File(prop.getProperty("appium")))
-                .withIPAddress("0.0.0.0").usingPort(4723).build();
+                .withIPAddress("0.0.0.0")
+                .usingPort(4723)
+                .build();
         service.start();
+    }
 
+    private void initializeDriver() throws URISyntaxException, MalformedURLException {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName(prop.getProperty("deviceName"));
         Path path = Paths.get("src/test/resources/ApiDemos-debug.apk");
         options.setApp(path.toAbsolutePath().toString());
-        AndroidDriver driver = new AndroidDriver(new URI("http://0.0.0.0:4723").toURL(), options);
-        driver.quit();
-        service.stop();
+        driver = new AndroidDriver(new URI("http://0.0.0.0:4723").toURL(), options);
+    }
+
+    @Test
+    public void appiumTest() {
+        // Add your test logic here
+        System.out.println("Running Appium test...");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+        if (service != null) {
+            service.stop();
+        }
+        stopEmulator();
+    }
+
+    private void stopEmulator() {
         ProcessBuilder closeEmulator = new ProcessBuilder("adb", "emu", "kill");
         try {
             closeEmulator.start();
         } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException("Failed to stop emulator", e);
         }
     }
 }
