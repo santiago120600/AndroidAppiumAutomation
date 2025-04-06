bd069482a14e4c986156fccde4c0987ea1e799a0
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-04-05 19:18:23 -0600

    add explicit wait in base page

--- src/main/java/com/appium/pageObjects/android/BasePageObject.java
+++ src/main/java/com/appium/pageObjects/android/BasePageObject.java
@@ -1,27 +1,33 @@
 package com.appium.pageObjects.android;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.android.AndroidDriver;
 
 public class BasePageObject {
 
     protected AndroidDriver driver;
+	private WebDriverWait wait;
 
     public BasePageObject(AndroidDriver driver) {
         this.driver = driver;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 	
 	protected void click(WebElement element) {
-		element.click();
+		wait.until(ExpectedConditions.elementToBeClickable(element)).click();
 	}
 	
 	protected void type(WebElement element, String text) {
-		element.sendKeys(text);
+		wait.until(ExpectedConditions.visibilityOf(element)).sendKeys(text);
 	}
 	
 	protected String getText(WebElement element) {
-		return element.getText();
+		return wait.until(ExpectedConditions.visibilityOf(element)).getText();
 	}
 
 }
